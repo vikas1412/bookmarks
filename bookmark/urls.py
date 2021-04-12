a1b7a665d2fabd6497e57a94ade0e069bf65cbc6
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-12 16:00:43 +0530

    Add url, empty static dir for Heroku

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,10 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bookmarks/', include('homepage.urls')),
 
     path('', RedirectView.as_view(url="bookmarks/", permanent=True)),
-]
+
+    path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
