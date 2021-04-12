6d43a4c037bffb7b85a750ecf43b63b1f67881d0
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-12 15:39:17 +0530

    Update url path

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from homepage import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    # path('', views.index, name="index"),
+
+
+    path('', views.index, name="index-bookmark"),
+    path('new/', views.new_update_bookmark, name="new-bookmark"),
+
+    path('<int:pk>/update/', views.new_update_bookmark, name="update-bookmark"),
+
+    path('<int:pk>/deleted/', views.delete_bookmark, name="delete-bookmark"),
+
+
 ]
