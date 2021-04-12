cea8f70e90efa58ce6e9beb3c8621bfc550dcc6c
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-12 12:02:01 +0530

    Register model

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from homepage.models import Bookmark
+
+
+@admin.register(Bookmark)
+class BookmarkInstanceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url', 'timestamp')
