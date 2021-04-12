a2bc504633dbca548b416f8e7038990e24f8c3d0
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-12 15:39:54 +0530

    Change home path to bookmarks/

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('homepage/', include('homepage.urls')),
+    path('bookmarks/', include('homepage.urls')),
 
-    path('', RedirectView.as_view(url="homepage/", permanent=True)),
+    path('', RedirectView.as_view(url="bookmarks/", permanent=True)),
 ]
