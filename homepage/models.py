364b0bac595c8de6c01af0f2dd798e60a4d65ed3
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-13 18:38:27 +0530

    Add Folder model, add new_folder attr

--- homepage/models.py
+++ homepage/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from taggit.managers import TaggableManager
 
 
+class Folder(models.Model):
+    name = models.CharField(max_length=200, default=None, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Bookmark(models.Model):
     name = models.CharField(max_length=200, default=None, blank=True)
+    folder_name = models.ManyToManyField(Folder, default=None, blank=True)
     url = models.URLField(max_length=230, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True, blank=True, null=True)
